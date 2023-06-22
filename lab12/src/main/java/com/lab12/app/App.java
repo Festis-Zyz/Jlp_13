18c125fe0eaf0f5d815277c899536323e72cd436
Festis-Zyz <zyzdmitri1@gmail.com>
2023-06-22 11:14:04 +0300

    init task1

--- lab12/src/main/java/com/lab12/app/App.java
+++ lab12/src/main/java/com/lab12/app/App.java
@@ -5,7 +5,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Dima!" );
-        Tester test = new Tester("Dima", "Zyz", 3);
-        test.checkSalary();
     }
 }
